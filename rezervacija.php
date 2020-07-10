d691936ef9989e5d592f6ce8bffdc2043fd73e40
Thracian42 <67063956+Thracian42@users.noreply.github.com>
2020-07-10 20:48:37 +0200

    Add files via upload
    jebem ti ljebac Bogovi

---
+++ rezervacija.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rezervacije</title>
+	<link rel="stylesheet" type="text/css" href="rez_stil.css">
+</head>
+<body>
+<h1 align="center">Rezervacije</h1>
+<br><br>
+<hr>
+<br><br>
+<p align="center">Best rates are guaranteed</p>
+<br><br>
+<p align="center">Hotel Cacak se trudi da vas ugosti da uzivate u svom ostanku</p>
+<br><br>
+
+<form>
+	<ul>
+
+		<li>
+			Dolazak: <br>
+			<input type="date" name="dolazak">
+		</li>
+
+		<li>
+			Odlazak: <br>
+			<input type="date" name="odlazak">
+		</li>
+
+		<li>
+			Odrasli: <br>
+			<input type="text" name="odrasli">
+		</li>
+
+		<li>
+			Promo kod: <br>
+			<input type="text" name="kod" placeholder="Unesite promo kod:">
+		</li>
+		<li>
+			<input type="submit" name="potvrda"><br>
+		</li>
+
+	</ul>
+
+</form>
+</body>
+</html>
